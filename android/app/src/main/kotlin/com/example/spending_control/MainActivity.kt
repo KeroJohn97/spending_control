51967bc07bc55ffa28fb72409ecb8cb7aa34ebd9
KeroJohn97 <keechung4@gmail.com>
2024-05-03 17:19:27 +0800

    fix: reset project with flutter v3.19.6

--- android/app/src/main/kotlin/com/example/spending_control/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/spending_control/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.spending_control
+package com.kerojohn.spending_control
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
